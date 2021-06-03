2b251b96e656b7c745da50d75fae52823c3ad479
kostitsyn <kostitsin.a@mail.ru>
2021-06-03 16:12:15 +0400

    Третье домашнее задание

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 class GoodsItem {
-	constructor(title, price) {
-		this.title = title;
+	constructor(id_product, product_name, price) {
+		this.id_product = id_product;
+		this.product_name = product_name;
 		this.price = price;
+
 	}
 
 	makeHtmlElem() {
-		return `<div class='goods-item p-3 bg-light border-warning mx-3'><h3>${this.title}</h3><p>${this.price}</p></div>`;
+		return `<div class='goods-item p-3 bg-light border-warning mx-3' itemid=${this.id_product}><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
 	}
 }
 
@@ -13,18 +15,21 @@ class GoodsList {
 	constructor() {
 		this.goods = [];
 	}
-	fetchGoods() {
-		this.goods = [
-			{title: 'Shirt', price: 150 },
-			{title: 'Socks', price: 50 },
-			{title: 'Jacket', price: 350 },
-			{title: 'Shoes', price: 250 },
-		];
+	async fetchGoods() {
+		const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
+		const response = await fetch(`${API_URL}/catalogData.json`);
+		if (response.ok) {
+			const catalogItems = await response.json();
+			console.log(catalogItems);
+			this.goods = catalogItems;
+		} else {
+			alert('Ошибка соединения с сервером');
+		}
 	}
 	renderToHtml() {
 		let listHtml = '';
 		this.goods.forEach(good => {
-			const goodItem = new GoodsItem(good.title, good.price);
+			const goodItem = new GoodsItem(good.id_product, good.product_name, good.price);
 			listHtml += goodItem.makeHtmlElem();
 		});
 		document.querySelector('.goods-list').innerHTML = listHtml; 
@@ -35,7 +40,7 @@ class GoodsList {
 			totalPrice += good.price;
 		})
 		let goodsElem = document.querySelector('.goods-list');
-		goodsElem.insertAdjacentHTML('afterend', `<p>Общая стоимость товаров ${totalPrice}`);  
+		goodsElem.insertAdjacentHTML('afterend', `<p>Общая стоимость товаров ${totalPrice}</p>`);  
 	}
 }
 
@@ -47,9 +52,9 @@ class BasketList {
 
 }
 
-const init = () => {
+const init = async () => {
 	const goodsObj = new GoodsList();
-	goodsObj.fetchGoods();
+	await goodsObj.fetchGoods();
 	goodsObj.renderToHtml();
 	goodsObj.countTotalPrice(); 
 }
