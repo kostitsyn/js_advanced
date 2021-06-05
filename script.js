2e83ab679a52e7d36c5f6e9c784e3e75c0266e73
kostitsyn <kostitsin.a@mail.ru>
2021-06-05 13:08:07 +0400

    Третье домашнее задание

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 class GoodsItem {
 	constructor(id_product, product_name, price) {
 		this.id_product = id_product;
@@ -70,6 +71,11 @@ class GoodsList {
 		return this.goods;
 	}
 
+	/**
+	 * Получить объект товара по его id.
+	 * @param  {integer} productId Id товара.
+	 * @return {object}  Объект товара.
+	 */
 	getElementById(productId) {
 		return this.goods.find(product => product.id_product == productId);
 	}
@@ -87,17 +93,29 @@ class BasketItem {
 	 * @return {string} Блок товара в строковом представлении.
 	 */
 	makeHtmlElem() {
-		return `<div class='goods-item'>
-					<h3 class='goods-name'>${this.product.product_name}</h3>
-					<p class='goods-price'>Цена: ${this.product.price * this.quantity}</p>
-					<p class='goods-price'>Количество: ${this.quantity}</p>
-					<input type="number" value=${this.quantity}>
+		return `<div class='basket-item'>
+					<h3 class='basket-product-name'>${this.product.product_name}</h3>
+					<p class='basket-product-price'>${this.getTotalPriceForItem()}</p>
+					<p class='basket-product-quantity'>Количество:</p>
+					<input name=${this.product.id_product} type="number" value=${this.quantity}><br><br>
 					<button name=${this.product.id_product} class="btn btn-danger btn-sm remove-basket-btn">Удалить</button>
 				</div>`;
 	}
 
-	updateQuantity(quantity) {
+	/**
+	 * Получить общую стоимость на одну позицию элемента корзины.
+	 * @return {string} Строка с общей стоимостью элемента корзины.
+	 */
+	getTotalPriceForItem() {
+		return `Цена: ${this.product.price * this.quantity}`;
+	}
 
+	/**
+	 * Обновить количество товара в корзине.
+	 * @param  {integer} quantity Новое кол-во товара.
+	 */
+	updateQuantity(quantity) {
+		this.quantity = quantity;
 	}
 }
 
@@ -145,33 +163,56 @@ class BasketList {
 	 */
 	renderToHtml() {
 		let listHtml = '';
-		this.goodsArr.forEach(basketProduct => {
-			/*const goodItem = new GoodsItem(basketProduct.product, good.product_name, good.price);*/
-			listHtml += basketProduct.makeHtmlElem();
-		});
-		document.querySelector('.modal-body').innerHTML = listHtml; 
+		if (this.goodsArr.length) {
+			this.goodsArr.forEach(basketProduct => {
+				listHtml += basketProduct.makeHtmlElem();
+			});
+		}else {
+			listHtml = this.getMessageOfEmtpyBasket();
+		}
+		document.querySelector('.modal-body').innerHTML = listHtml + `${this.goodsArr.length ? `<p class="basket-total-price">${this.getTotalPrice()}</p>` : ''}`; 
 	}
 
+	/**
+	 * Получить объект товара по id.
+	 * @param  {integer} productId id товара.
+	 * @return {object} Объект товара.
+	 */
 	getElementById(productId) {
 		return this.goodsArr.find(basketProduct => basketProduct.product.id_product == productId);
 	}
+
+	/**
+	 * Получить сообщение о пустой корзине.
+	 * @return {string} Строка с сообщением.
+	 */
+	getMessageOfEmtpyBasket() {
+		return 'Товаров в корзине пока нет.';
+	}
+
+	/**
+	 * Получить общую стоимость товаров в корзине.
+	 * @return {string} Строка с общей стоимостью товаров в корзине.
+	 */
+	getTotalPrice() {
+		let totalPrice = 0;
+		this.goodsArr.forEach(basketProduct => {
+			totalPrice += basketProduct.quantity * basketProduct.product.price;
+		})
+		return `Общая стоимость товаров: ${totalPrice}`;
+	}	
 }
 
-
-let loadIcon = document.createElement('i');
+// Добавление спиннера при загрузке страницы
+/*let loadIcon = document.createElement('i');
 	loadIcon.classList.add('fas', 'fa-spinner', 'fa-spin');
 	document.querySelector('.main-container').insertAdjacentElement('afterbegin', loadIcon);
 
 	window.addEventListener('load', event => {
 		loadIcon.style.display = 'none';
-	})
+	})*/
 
 const init = async () => {
-
-	/*let mainContainerEl = document.querySelector('.main-container');*/
-
-	
-
 	const goodsObj = new GoodsList;
 	await goodsObj.fetchGoods();
 	goodsObj.renderToHtml();
@@ -191,13 +232,30 @@ const init = async () => {
 	let basketButton = document.getElementsByName('basket-btn')[0];
 	basketButton.addEventListener('click', event => {
 		basketListObj.renderToHtml();
+		basketListObj.getTotalPrice();
 
 		let removeBasketBtns = document.querySelectorAll('.remove-basket-btn');
 		removeBasketBtns.forEach(btn => {
 			btn.addEventListener('click', event => {
-				const productItem = basketListObj.getElementById(event.target.name);
-				basketListObj.removeProduct(productItem);
-				basketListObj.renderToHtml();
+				const basketItem = basketListObj.getElementById(event.target.name);
+				basketListObj.removeProduct(basketItem);
+				let basketItemElem = event.target.parentNode;
+				basketItemElem.style.display = 'none';
+				if (!basketListObj.getBasketArr().length) {
+					basketItemElem.parentNode.innerText = basketListObj.getMessageOfEmtpyBasket();
+				}
+				basketItemElem.parentNode.querySelector('.basket-total-price').innerText = basketListObj.getTotalPrice();
+			})
+		})
+
+		
+		let basketItemInputs = document.querySelectorAll('.basket-item');
+		basketItemInputs.forEach(inputItem => {
+			inputItem.addEventListener('change', event => {
+				const basketItem = basketListObj.getElementById(event.target.name);
+				basketItem.updateQuantity(+event.target.value);
+				event.path[1].querySelector('.basket-product-price').innerText = basketItem.getTotalPriceForItem();
+				event.currentTarget.parentNode.querySelector('.basket-total-price').innerText = basketListObj.getTotalPrice();
 			})
 		})
 	})
@@ -214,6 +272,7 @@ const init = async () => {
 			event.target.classList.remove('pulse');
 		})
 	})
+
 }
 
 window.onload = init;
