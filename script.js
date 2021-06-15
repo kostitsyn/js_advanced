f88ab454c1e9a25aa463c04e171ad8cb640c9f20
kostitsyn <kostitsin.a@mail.ru>
2021-06-15 12:48:38 +0400

    Шестое домашнее задание

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ const app = new Vue({
 		 * Получить данные о товарах с сервера
 		 */
 		async fetchGoods() {
-			const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutoria/online-store-api/master/responses/';
+			const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
 			const response = await fetch(`${API_URL}/catalogData.json`);
 
 			if (response.ok) {
