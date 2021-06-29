9059072fbc40d22453e36589378ccf18e24c22d1
kostitsyn <kostitsin.a@mail.ru>
2021-06-29 19:12:38 +0400

    Восьмое домашнее задание

--- components/basket_list.js
+++ components/basket_list.js
@@ -2,7 +2,7 @@ export const basketList = Vue.component('basket-list', {
 		props: ['basketArr', 'getPrice', 'getTotalPriceInBasket'],
 		template:  `<div>
 						<div class="basket-list row-cols-lg-5 d-flex justify-content-center flex-wrap">
-					       <basket-item v-for="basketItem in basketArr" :get-price="getPrice" :product="basketItem"></basket-item>
+					       <basket-item v-for="basketItem in basketArr" :get-price="getPrice" :product="basketItem" :key="basketItem.id"></basket-item>
 					    </div>
 					    <p class="position-static goods-total-price text-center">{{ getTotalPriceInBasket() }}</p>
 					</div>`,
--- components/goods_list.js
+++ components/goods_list.js
@@ -1,7 +1,7 @@
 export const goodsList = Vue.component('goods-list', {
 	props: ['goods', 'getTotalPrice'],
 	template: `<div class="goods-list row-cols-lg-5 d-flex justify-content-center mb-5 flex-wrap">
-			       <goods-item v-for="goodItem in goods" :product="goodItem"></goods-item>
+			       <goods-item v-for="goodItem in goods" :product="goodItem" :key="goodItem.id"></goods-item>
 			       <p class="goods-total-price">{{ getTotalPrice() }}</p>
 			   </div>`,
 
